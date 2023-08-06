749b9e3c4fb3788e65269d25a56b491235ab1229
tunglam123543 <lam282649@gmail.com>
2023-08-06 14:43:23 +0700

    new update

---
+++ java.js
@@ -0,0 +1,41 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+/////////////////////quang cao////////////////////////
+
+let advertise = document.querySelector(".advertise");
+        let btn = document.getElementById("btn");
+        btn.addEventListener("click", function () {
+          if (advertise.style.display == "none") {
+            advertise.style.display = "block";
+          } else {
+            advertise.style.display = "none";
+          }
+        });
+
+
+
